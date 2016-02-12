5d6290b95177a608ea65ddfa7cecce1df73fbb89
Frank Mitchell <me@frankmitchell.org>
2016-02-12 14:35:22 -0500

    Ensure the Conqueso API acks POST requests.

--- test/conqueso-api-v1.js
+++ test/conqueso-api-v1.js
@@ -1,7 +1,10 @@
 /* eslint-env mocha */
 'use strict';
 
+const request = require('supertest');
+
 const testServerPort = 3000;
+const HTTP_OK = 200;
 
 /**
  * Create a new Express server for testing
@@ -24,5 +27,25 @@ describe('Conqueso API v1', () => {
 
   afterEach((done) => {
     server.close(done);
+  });
+
+  it('acknowledges POST requests', (done) => {
+    const properties = {
+      "instanceMetaData": {
+        "meta.property.1": "songs you've never heard of",
+        "meta.property.2": "artisanal cream cheese"
+      },
+      "properties": [{
+        "name": "hipster-mode-enabled",
+        "value": "true",
+        "type": "BOOLEAN",
+        "description": "Are you wearing skinny jeans?"
+      }]
+    }
+
+    request(server)
+      .post('/v1/conqueso/api/roles/search/properties')
+      .send(properties)
+      .expect(HTTP_OK, '', done);
   });
 });

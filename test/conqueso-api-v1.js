6fe95c838a41c05882e325a715e9115b980ecb51
Frank Mitchell <me@frankmitchell.org>
2016-02-12 14:40:23 -0500

    Ensure Conqueso API acks PUT requests.

--- test/conqueso-api-v1.js
+++ test/conqueso-api-v1.js
@@ -5,6 +5,18 @@ const request = require('supertest');
 
 const testServerPort = 3000;
 const HTTP_OK = 200;
+const conquesoProperties = {
+  "instanceMetaData": {
+    "meta.property.1": "songs you've never heard of",
+    "meta.property.2": "artisanal cream cheese"
+  },
+  "properties": [{
+    "name": "hipster-mode-enabled",
+    "value": "true",
+    "type": "BOOLEAN",
+    "description": "Are you wearing skinny jeans?"
+  }]
+}
 
 /**
  * Create a new Express server for testing
@@ -30,22 +42,16 @@ describe('Conqueso API v1', () => {
   });
 
   it('acknowledges POST requests', (done) => {
-    const properties = {
-      "instanceMetaData": {
-        "meta.property.1": "songs you've never heard of",
-        "meta.property.2": "artisanal cream cheese"
-      },
-      "properties": [{
-        "name": "hipster-mode-enabled",
-        "value": "true",
-        "type": "BOOLEAN",
-        "description": "Are you wearing skinny jeans?"
-      }]
-    }
-
     request(server)
       .post('/v1/conqueso/api/roles/search/properties')
-      .send(properties)
+      .send(conquesoProperties)
+      .expect(HTTP_OK, '', done);
+  });
+
+  it('acknowledges PUT requests', (done) => {
+    request(server)
+      .put('/v1/conqueso/api/roles/search/properties')
+      .send(conquesoProperties)
       .expect(HTTP_OK, '', done);
   });
 });

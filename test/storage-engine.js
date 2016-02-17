523c0569c2f4f026c805862ecabe3cc9b84be9db
Frank Mitchell <me@frankmitchell.org>
2016-02-17 14:34:09 -0500

    Allows users to update the storage engine and get events.

--- test/storage-engine.js
+++ test/storage-engine.js
@@ -2,7 +2,8 @@
 'use strict';
 
 const should = require('should');
-
+const assert = require('assert');
+const EventEmitter = require('events');
 const Storage = require('../lib/storage');
 
 describe('Storage Engine', () => {
@@ -10,6 +11,25 @@ describe('Storage Engine', () => {
     const storage = new Storage();
 
     storage.should.have.property('properties');
-    should(storage.properties).be.empty;
+    should(storage.properties).be.empty();
+  });
+
+  it('fires update events with new properties', (done) => {
+    const emitter = new EventEmitter();
+    const storage = new Storage(emitter);
+
+    const updateTimeoutMS = 5000;
+    const updateTimeout = setTimeout(() => {
+      assert(false, 'Storage#update event never fired.');
+      done();
+    }, updateTimeoutMS);
+
+    emitter.on('update', (properties) => {
+      clearTimeout(updateTimeout);
+      should(properties).be.empty();
+      done();
+    });
+
+    storage.update();
   });
 });

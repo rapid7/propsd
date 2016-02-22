4dc31780cbc416aabc56d5ae9591eda25321e72b
Frank Mitchell <me@frankmitchell.org>
2016-02-22 14:50:59 -0500

    Provide a base server binary.

---
+++ server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict';
+
+const express = require('express');
+const http = require('http');
+
+const config = require('./lib/config').load();
+const logger = require('./lib/logger').attach(config);
+
+const host = config.get('service:hostname');
+const port = config.get('service:port');
+const app = express();
+const server = http.createServer(app);
+
+server.listen(port, host, () => {
+  logger.info('Listening on ' + host + ':' + port);
+});

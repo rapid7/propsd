7456d55fc38987f0b0315d0f28accdd899b878e7
Frank Mitchell <me@frankmitchell.org>
2016-02-22 15:25:24 -0500

    Use common "--config" argument for loading configuration.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 
 const args = require('yargs')
   .usage('Usage: $0 [args]')
-  .option('cf', {
-    alias: 'config-file',
-    describe: 'path to a configuration file',
+  .option('c', {
+    alias: 'config',
+    describe: 'Load configuration from file',
     type: 'string'
   })
   .help('help')

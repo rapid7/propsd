ebaa13313342c352bb5cbbb1034f1d209a06b125
Frank Mitchell <me@frankmitchell.org>
2016-02-23 09:47:04 -0500

    Expose Config and Log as global objects.

--- bin/server.js
+++ bin/server.js
@@ -14,11 +14,11 @@ const args = require('yargs')
 const express = require('express');
 const http = require('http');
 
-const config = require('../lib/config').load(args.cf);
-const logger = require('../lib/logger').attach(config);
+global.Config = require('../lib/config').load(args.cf);
+global.Log = require('../lib/logger').attach(global.Config);
 
-const host = config.get('service:hostname');
-const port = config.get('service:port');
+const host = Config.get('service:hostname');
+const port = Config.get('service:port');
 const app = express();
 const server = http.createServer(app);
 
@@ -26,5 +26,5 @@ require('../lib/control/v1/core').attach(app);
 require('../lib/control/v1/conqueso').attach(app);
 
 server.listen(port, host, () => {
-  logger.info('Listening on ' + host + ':' + port);
+  Log.info('Listening on ' + host + ':' + port);
 });

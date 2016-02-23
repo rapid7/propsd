6d27a3d0a06e1a7ea030c11f3e961ab9e24403f5
Frank Mitchell <me@frankmitchell.org>
2016-02-23 09:45:14 -0500

    Move server.js into the bin directory.

--- bin/server.js
+++ bin/server.js
@@ -14,16 +14,16 @@ const args = require('yargs')
 const express = require('express');
 const http = require('http');
 
-const config = require('./lib/config').load(args.cf);
-const logger = require('./lib/logger').attach(config);
+const config = require('../lib/config').load(args.cf);
+const logger = require('../lib/logger').attach(config);
 
 const host = config.get('service:hostname');
 const port = config.get('service:port');
 const app = express();
 const server = http.createServer(app);
 
-require('./lib/control/v1/core').attach(app);
-require('./lib/control/v1/conqueso').attach(app);
+require('../lib/control/v1/core').attach(app);
+require('../lib/control/v1/conqueso').attach(app);
 
 server.listen(port, host, () => {
   logger.info('Listening on ' + host + ':' + port);

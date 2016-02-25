75b091653a75e26b5a00ac5f87039c8562aed148
Dave Greene <dgreene@rapid7.com>
2016-02-25 14:00:15 -0500

    First pass at plugin-manager with test stubs.

---
+++ test/plugin-manager.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+'use strict';
+
+const should = require('should');
+
+describe('Plugin manager', () => {
+  it('fetches the index from S3 and gets a list of sources', () => {
+
+  });
+
+  it('registers plugins with the storage engine', () => {
+
+  });
+
+  it('sets event handlers for plugin events', () => {
+
+  });
+
+  it('updates the storage engine when a plugin emits an update event', () => {
+
+  });
+});

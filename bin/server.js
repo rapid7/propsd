c21eedde233b3c98e1d665134c40387d40f46a76
davepgreene <davepgreene@gmail.com>
2016-03-04 12:03:13 -0500

    Updated server to add plugin manager initialization.

--- bin/server.js
+++ bin/server.js
@@ -18,14 +18,27 @@ const http = require('http');
 
 global.Config = require('../lib/config').load(args.c);
 global.Log = require('../lib/logger').attach(global.Config);
+const PluginManager = require('../lib/plugin-manager');
 
 const host = Config.get('service:hostname');
 const port = Config.get('service:port');
 const app = express();
 const server = http.createServer(app);
+const storage = new (require('../lib/storage'))();
+const manager = new PluginManager(storage);
 
-require('../lib/control/v1/core').attach(app);
-require('../lib/control/v1/conqueso').attach(app);
+manager.on('error', (err) => {
+  Log.error(err);
+});
+
+server.on('error', (err) => {
+  Log.error(err);
+});
+
+manager.initialize();
+
+require('../lib/control/v1/core').attach(app, storage, manager);
+require('../lib/control/v1/conqueso').attach(app, storage);
 
 server.listen(port, host, () => {
   Log.info('Listening on ' + host + ':' + port);

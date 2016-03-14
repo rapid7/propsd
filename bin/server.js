ea9d5e0f559c31b3b8a9c0893d7e4be738fadd75
davepgreene <davepgreene@gmail.com>
2016-03-14 16:57:10 -0400

    Registered middleware with express app. Restructured server.js slightly so that it's easier to visually parse. Resolves #98.

--- bin/server.js
+++ bin/server.js
@@ -16,38 +16,43 @@ const args = require('yargs')
 const express = require('express');
 const http = require('http');
 const Path = require('path');
+const Logger = require('../lib/logger');
+const app = express();
 
 // Load nconf into the global namespace
 global.Config = require('nconf')
     .env()
     .argv();
+
 if (args.c) {
   global.Config.file(Path.resolve(process.cwd(), args.c));
 }
 global.Config.defaults(require('../config/defaults.json'));
 
-global.Log = require('../lib/logger').attach(global.Config);
+// Set up logging
+global.Log = Logger.attach(global.Config);
+
+// Add request logging middleware
+app.use(Logger.logRequests({write: (message) => global.Log.info(message)}));
+
+// Initialize the Plugin Manager and storage layer
 const PluginManager = require('../lib/plugin-manager');
-
-const host = Config.get('service:hostname');
-const port = Config.get('service:port');
-const app = express();
-const server = http.createServer(app);
 const storage = new (require('../lib/storage'))();
-const manager = new PluginManager(storage);
+const manager = new PluginManager(storage, {});
 
-manager.on('error', (err) => {
-  Log.error(err);
-});
-
-server.on('error', (err) => {
-  Log.error(err);
-});
-
+manager.on('error', (err) => global.Log.error(err));
 manager.initialize();
 
+// Register endpoints
 require('../lib/control/v1/core').attach(app, storage, manager);
 require('../lib/control/v1/conqueso').attach(app, storage);
+
+// Instantiate server and start it
+const host = Config.get('service:hostname');
+const port = Config.get('service:port');
+const server = http.createServer(app);
+
+server.on('error', (err) => global.Log.error(err));
 
 server.listen(port, host, () => {
   Log.info(`Listening on ${host}:${port}`);

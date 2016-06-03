049ed54604ceeed50a694ba2e25e0dfeaa1dd2a4
John Manero <jmanero@rapid7.com>
2016-06-03 14:31:44 -0400

    Update server executable to use Properties and Soruces

--- bin/server.js
+++ bin/server.js
@@ -15,50 +15,61 @@ const args = require('yargs')
 
 const deprecate = require('depd')('propsd');
 const express = require('express');
-const http = require('http');
+const HTTP = require('http');
 const Path = require('path');
 const Logger = require('../lib/logger');
+
+const Properties = require('../lib/properties');
+const Sources = require('../lib/sources');
+const Metadata = require('../lib/source/metadata');
+const S3 = require('../lib/source/s3');
+
 const app = express();
+const server = HTTP.createServer(app);
 
 // Load nconf into the global namespace
-global.Config = require('nconf')
-    .env()
-    .argv();
+global.Config = require('nconf').env()
+  .argv({
+    config: {
+      alias: 'c',
+      default: '/etc/propsd/config.json',
+      describe: 'Path to local propsd configuration'
+    }
+  });
 
-if (args.c) {
-  global.Config.file(Path.resolve(process.cwd(), args.c));
-}
-global.Config.defaults(require('../config/defaults.json'));
+if (args.c) { Config.file(Path.resolve(process.cwd(), args.c)); }
+Config.defaults(require('../config/defaults.json'));
 
-// Set up logging
-global.Log = Logger.attach(global.Config.get('log:level'), global.Config.get('log:filename'));
+global.Log = Logger.attach(Config.get('log:level'), Config.get('log:filename'));
 
 // Add request logging middleware
-if (global.Config.get('log:access')) {
+if (Config.get('log:access')) {
   deprecate('Separate logging control for access logs has been deprecated and will be removed in a later version.');
 }
 
-app.use(Logger.requests(global.Log, global.Config.get('log:access:level') || global.Config.get('log:level')));
+app.use(Logger.requests(Log, Config.get('log:access:level') || Config.get('log:level')));
 
-// Initialize the Plugin Manager and storage layer
-const PluginManager = require('../lib/plugin-manager');
-const storage = new (require('../lib/storage'))();
-const manager = new PluginManager(storage, {});
+const properties = new Properties();
+const sources = new Sources(properties);
 
-manager.on('error', (err, logMetadata) => global.Log.log('ERROR', err, logMetadata));
-manager.initialize();
+// Add metadata and some statics
+properties.static(Config.get('properties'));
+properties.dynamic(new Metadata(Config.get('metadata')), 'instance');
+
+// Create the Index source
+sources.index(new S3('index', Config.get('index')));
+
+// Go!
+sources.initialize();
 
 // Register endpoints
-require('../lib/control/v1/core').attach(app, storage, manager);
-require('../lib/control/v1/conqueso').attach(app, storage);
+require('../lib/control/v1/core').attach(app, sources);
+require('../lib/control/v1/conqueso').attach(app, properties);
 
 // Instantiate server and start it
 const host = Config.get('service:hostname');
 const port = Config.get('service:port');
-const server = http.createServer(app);
-
-server.on('error', (err) => global.Log.log('ERROR', err));
 
 server.listen(port, host, () => {
-  global.Log.log('INFO', `Listening on ${host}:${port}`);
+  Log.log('INFO', `Listening on ${host}:${port}`);
 });

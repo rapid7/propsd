ea20e2a6e18dcf02af822cfe28558bf7c100797d
Frank Mitchell <me@frankmitchell.org>
2016-09-02 12:09:35 -0400

    First pass at adding transformation logic for $tokend parsing.

--- test/tokend-transformer.js
+++ test/tokend-transformer.js
@@ -1,9 +1,43 @@
 'use strict';
 
 const expect = require('chai').expect;
+const nock = require('nock');
 const TokendTransformer = require('../lib/transformers/tokend');
 
 describe('TokendTransformer', function () {
+  it('transforms $tokend properties', function (done) {
+    const untransformedProperties = {
+      password: {
+        $tokend: {
+          type: 'secret',
+          resource: '/v1/secret/default/kali/root/password'
+        }
+      }
+    };
+
+    nock('http://token.d:4500')
+      .get('/v1/secret/default/kali/root/password')
+      .reply(200, {
+        secret: 'toor'
+      });
+
+    const transformer = new TokendTransformer({
+      host: 'token.d',
+      port: 4500
+    });
+
+    transformer.transform(untransformedProperties)
+      .then((transformedProperties) => {
+        expect(untransformedProperties).to.not.eql(transformedProperties);
+        expect(transformedProperties).to.eql({
+          password: 'toor'
+        });
+
+        done();
+      })
+      .catch(done);
+  });
+
   it('ignores non-$tokend properties', function (done) {
     const untransformedProperties = {
       key: 'value'

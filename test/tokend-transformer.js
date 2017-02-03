38ddd4ba5666426416edf9f9cd73c60ec2b20c0c
davepgreene <davepgreene@gmail.com>
2017-02-03 11:38:03 -0500

    Implemented tokend transformer for KMS encrypted secrets.

--- test/tokend-transformer.js
+++ test/tokend-transformer.js
@@ -135,6 +135,40 @@ describe('TokendTransformer', function () {
     .catch(done);
   });
 
+  it('transforms KMS $tokend properties', function (done) {
+    const untransformedProperties = {
+      password: {
+        $tokend: {
+          type: 'kms',
+          resource: '/v1/kms/decrypt',
+          ciphertext: 'gbbe'
+        }
+      }
+    };
+
+    const tokend = nock('http://127.0.0.1:4500')
+        .post('/v1/kms/decrypt', {
+          ciphertext: 'gbbe'
+        })
+        .reply(200, {
+          plaintext: 'toor',
+          keyid: 'arn:aws:kms:region:account-id:key/key-id'
+        });
+
+    _transformer = new TokendTransformer();
+
+    _transformer.transform(untransformedProperties)
+        .then((transformedProperties) => {
+          expect(transformedProperties).to.eql({
+            password: 'toor'
+          });
+
+          tokend.done();
+          done();
+        })
+        .catch(done);
+  });
+
   it('transforms nested $tokend properties', function (done) {
     const untransformedProperties = {
       database: {

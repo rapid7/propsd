64c75fbf09bd777fbebde5d874ba5ee0dc8788a4
aung-r7 <aung@rapid7.com>
2019-06-05 16:34:57 -0400

    PD-20473: Changed cookbook back to node 4.8.2 and updated checksum

--- cookbook/recipes/nodejs.rb
+++ cookbook/recipes/nodejs.rb
@@ -8,7 +8,7 @@
 # in the MIT license are reserved. See the included LICENSE file for more details.
 #
 
-node.default['nodejs']['version'] = '8.16.0'
-node.default['nodejs']['binary']['checksum'] = 'e538ffaaf2f808c084e70f1a1d2ff5559cff892cfd56e0bb67d00b0a95fc3a7a'
+node.default['nodejs']['version'] = '4.8.2'
+node.default['nodejs']['binary']['checksum'] = '4d4a37f980bb2770c44d7123864650d0823bae696d7db09d9ed83028cab32fd3'
 
 include_recipe 'nodejs::nodejs_from_binary'
